4b027fa277443c9bcd8c1e162ef9b1ddb07f8563
NickNedilko <mykola.ned34@gmail.com>
2024-04-24 15:23:08 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,18 +17,6 @@ const initialValues = [
 ]
 
 
-const salaries1 = {
-   Manager: { salary: 1000, tax: "10%" },
-   Designer: { salary: 600, tax: "30%" },
-  Artist: { salary: 1500, tax: "15%" },
-}
-   
-const team1 = [
-   { name: "Misha", specialization: "Manager" },
-   { name: "Max", specialization: "Designer" },
-   { name: "Vova", specialization: "Designer"},
-  { name: "Leo", specialization: "Artist" },]
-   
 
 
 function App() {
